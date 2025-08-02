8fd7ba6863876294a1dedc565852ab9fd8fd84c6
Lima Eftekhar <limaeftkhar@gmail.com>
2025-08-02 01:35:59 -0500

    answers for lesson 1

--- 01-node-tutorial/02-globals.js
+++ 01-node-tutorial/02-globals.js
@@ -6,7 +6,14 @@
 // module     - info about current module (file)
 // process    - info about env where the program is being executed
 
+//print current directory path
 console.log(__dirname)
+// this print "Hi there" the environment variable
+console.log(process.env.MY_VAR)
+// this print file name
+console.log(__filename)
 setInterval(() => {
   console.log('hello world')
 }, 1000)
+
+
--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,21 @@
-console.log('Welcome to Node Tutorial')
+// console.log('Welcome to Node Tutorial')
+
+const http = require ('http')
+const server = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        res.end("Welcome to the home page")
+    }
+    else if(req.url === '/services'){
+        res.end("Welcome to the services page")
+    } else{
+
+    res.end(
+        `<h1>Ooops! the page you are looking for does not exist.</h1></b>
+        <a href = "/">HomePage</a>`
+    )}
+
+})
+
+server.listen(300)
+
+
